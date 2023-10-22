2b4e6dd979beb89f3e8f70fe2460c08adb1e8e62
Mauricio Roh <59180265+MauriRoh@users.noreply.github.com>
2023-10-22 20:26:45 -0300

    Test V1.0

---
+++ seleniumTest/test.py
@@ -0,0 +1,56 @@
+# import unittest
+# from pyunitreport import HTMLTestRunner
+import time
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.select import Select    # Para los elementos Select
+
+
+# # driver = webdriver.Firefox()
+# # driver = webdriver.Edge()
+# # driver = webdriver.Chrome()
+# driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+# driver.get(url='https://www.carrefour.com.ar/')
+# # driver.maximize_window()
+# print(driver.title)
+# driver.quit()
+# driver.close()
+
+class FindElementByXPath():
+
+    def ButtonAccess(self):
+        driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+        # driver.get(url='https://www.farmacity.com/')
+        # driver.get(url='https://www.sugarcrm.com/au/request-demo/')
+        driver.get(url='https://www.yatra.com/')
+        time.sleep(5)
+        driver.find_element(By.XPATH, "//span[normalize-space()='Ingresar']").click()
+        time.sleep(5)
+        driver.find_element(By.XPATH, "//input[@placeholder='ejemplo@mail.com']").send_keys('soft.organapp@gmail.com')
+        driver.find_element(By.XPATH, "//input[@placeholder='ejemplo@mail.com']").is_enabled() # si está activo un elemento
+        driver.find_element(By.XPATH, "//input[@placeholder='ejemplo@mail.com']").is_displayed() # si un elemento está presente en la página retorna True
+
+        driver.find_element(By.XPATH,"//input[@placeholder='ejemplo@mail.com']").click()  # Activas el CheckBox o RadioButton
+        driver.find_element(By.XPATH, "//input[@placeholder='ejemplo@mail.com']").is_selected() # CheckBox o RadioButton está seleccionado retorna True
+
+
+        time.sleep(15)
+        # driver.find_element("//input[@placeholder='ejemplo@mail.com']").send_keys('test@test.com')
+        # time.sleep(5)
+
+
+findElementByXPath = FindElementByXPath()
+print(findElementByXPath.ButtonAccess())
+
+
+# class PruebaTest(unittest.TestCase):
+#     def SetUp(self):
+#         self.driver = webdriver.Chrome(executable_path = "C://Users/HP/Documents/DriverAplications/chromedriver/chromedriver.exe")
+#
+#     def test_Prueba(self):
+#         pass
+#
+#     def tearDown(self):
+#         return super().tearDown()
