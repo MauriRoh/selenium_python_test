ac8b161cdbc25bba92b8f3f12cb85644da593bf5
Mauricio Roh <59180265+MauriRoh@users.noreply.github.com>
2023-10-22 21:10:12 -0300

    Test V1.1

--- seleniumTest/test.py
+++ seleniumTest/test.py
@@ -5,7 +5,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.select import Select    # Para los elementos Select
+from selenium.webdriver.support.select import Select    # Para los elementos Select - Dropdown
 
 
 # # driver = webdriver.Firefox()
@@ -24,16 +24,27 @@ class FindElementByXPath():
         driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
         # driver.get(url='https://www.farmacity.com/')
         # driver.get(url='https://www.sugarcrm.com/au/request-demo/')
-        driver.get(url='https://www.yatra.com/')
-        time.sleep(5)
-        driver.find_element(By.XPATH, "//span[normalize-space()='Ingresar']").click()
-        time.sleep(5)
-        driver.find_element(By.XPATH, "//input[@placeholder='ejemplo@mail.com']").send_keys('soft.organapp@gmail.com')
-        driver.find_element(By.XPATH, "//input[@placeholder='ejemplo@mail.com']").is_enabled() # si está activo un elemento
-        driver.find_element(By.XPATH, "//input[@placeholder='ejemplo@mail.com']").is_displayed() # si un elemento está presente en la página retorna True
+        # driver.get(url='https://www.yatra.com/')
+        driver.get(url='https://www.salesforce.com/au/form/signup/freetrial-sales/?d=topnav2-btn-ft')
 
-        driver.find_element(By.XPATH,"//input[@placeholder='ejemplo@mail.com']").click()  # Activas el CheckBox o RadioButton
-        driver.find_element(By.XPATH, "//input[@placeholder='ejemplo@mail.com']").is_selected() # CheckBox o RadioButton está seleccionado retorna True
+        time.sleep(5)
+        # driver.find_element(By.XPATH, "//span[normalize-space()='Ingresar']").click()
+        # time.sleep(5)
+        # driver.find_element(By.XPATH, "//input[@placeholder='ejemplo@mail.com']").send_keys('soft.organapp@gmail.com')
+        # driver.find_element(By.XPATH, "//input[@placeholder='ejemplo@mail.com']").is_enabled() # si está activo un elemento
+        # driver.find_element(By.XPATH, "//input[@placeholder='ejemplo@mail.com']").is_displayed() # si un elemento está presente en la página retorna True
+        #
+        # driver.find_element(By.XPATH,"//input[@placeholder='ejemplo@mail.com']").click()  # Activas el CheckBox o RadioButton
+        # driver.find_element(By.XPATH, "//input[@placeholder='ejemplo@mail.com']").is_selected() # CheckBox o RadioButton está seleccionado retorna True
+        driver.find_element(By.ID, "onetrust-accept-btn-handler").click()
+
+        # Selector Drowdown
+        selectDropdown = Select(driver.find_element(By.NAME, "CompanyEmployees"))  # Select - Dropdown
+        selectDropdown.select_by_index(1)
+        time.sleep(5)
+        selectDropdown.select_by_visible_text('1 - 15 employees')
+        time.sleep(5)
+        selectDropdown.select_by_value('30')
 
 
         time.sleep(15)
